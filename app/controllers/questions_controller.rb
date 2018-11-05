d772b14184f671394f9b1484a252d33222193987
Elizabeth Creary <baebaebun101@gmail.com>
2018-11-05 18:08:55 -0500

    answer page returns the answer and the question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,20 @@
 class QuestionsController < ApplicationController
+
   def ask
   end
 
   def answer
+    @answer = ""
+    @question = params[:question]
+    if @question.include? "?"
+      return @answer << "Silly question, get dressed and go to work!"
+
+    elsif @question == "I am going to work right now!"
+      return ""
+
+    else
+      return @answer << "I don't care, get dressed and go to work!"
+    end
   end
+
 end

ebb20619fdf7081930c2e8a4c26e219d558e2c32
Elizabeth Creary <baebaebun101@gmail.com>
2018-11-05 21:22:30 -0500

    The ask and answer are interchanging and responding alright

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,4 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
@@ -9,12 +8,13 @@ class QuestionsController < ApplicationController
     if @question.include? "?"
       return @answer << "Silly question, get dressed and go to work!"
 
-    elsif @question == "I am going to work right now!"
-      return ""
+    elsif @question == "I am going to work!"
+      return @answer << "Great!"
 
+    elsif @question.blank?
+      return @answer << "You didn't write anything! Go to work!"
     else
       return @answer << "I don't care, get dressed and go to work!"
     end
   end
-
 end
